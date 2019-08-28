535f30e3440a32ae407d8748ea79920f2142de30
Mmmmmrr <1539716914@qq.com>
2019-08-28 23:32:22 +0800

    day1(3)[斐波那契数、判断括号的合法性]

--- day1/day1/test.cpp
+++ day1/day1/test.cpp
@@ -131,3 +131,63 @@ public:
 		return symbol * sum; 
 	}
 };
+
+
+//Fibonacci数
+#include <iostream> 
+#include <algorithm>
+using namespace std; 
+int main()
+{
+	int N, f, l = 0,r = 0,f0 = 0,f1 = 1;
+	cin >> N;
+	while(1)
+	{
+		f = f0 + f1;
+		f0 = f1;
+		f1 = f; //找到比N小且距离N最近的数，求出距离 
+		if(f < N) 
+			l = N-f;
+		else
+		{
+			//找到比N大且距离N最近的数，求出距离 
+			r = f - N;
+			break;
+		}
+	} //取最小距离 
+	cout << min(l,r) << endl;
+	return 0;
+}
+
+
+//判断括号合法性
+#include <iostream>
+#include <stack>
+using namespace std;
+class Parenthesis {
+public: 
+	bool chkParenthesis(string A, int n) 
+	{
+		// write code here 
+		stack<char> sc; 
+		for (auto ele : A)
+		{
+			switch (ele) 
+			{
+			case '(':	
+				sc.push(ele); 
+				break;
+			case ')':{
+						 if (sc.empty() || sc.top() != '(')
+							 return false; 
+						 else
+							 sc.pop();
+			}
+				break; 
+			default:
+				return false;
+			}
+		}
+		return true;
+	}
+};
