85cb29910b5a37f43782d2c7c20f9f3123b77984
Mmmmmrr <1539716914@qq.com>
2019-09-05 22:39:40 +0800

    组成一个整数最接近的素数、参数解析

--- day1/day1/test.cpp
+++ day1/day1/test.cpp
@@ -510,3 +510,101 @@ int main()
 	}
 	return 0;
 }
+
+
+//二进制插入
+class BinInsert 
+{ 
+public: 
+	int binInsert(int n, int m, int j, int i)
+	{
+		m <<= j; 
+		return n | m;
+	}
+};
+
+
+//查找组成一个偶数最接近的素数
+#include<iostream>
+#include<algorithm>
+using namespace std; 
+//判断素数
+bool isPrime(int num) 
+{
+	int tmp = sqrt(num); 
+	//只遍历到开方出，不能被开方左边的数整除，则一定不能被开方右边的数整除
+	for (int i = 2; i <= tmp; i++) 
+	{
+		if (num %i == 0) 
+			return false; 
+	}
+	return true; 
+}
+int main()
+{
+	int num;
+	int half;
+	int i; 
+	while (cin >> num) 
+	{ 
+		half = num / 2;
+		//从中间向两边找 
+		for (i = half; i > 0; i--)
+		{ 
+			if (isPrime(i) && isPrime(num - i))
+				break; 
+		}
+		cout << i << endl << num - i << endl;
+	}
+	return 0;
+}
+
+
+//参数解析
+#include <iostream> 
+#include <string>
+using namespace std;
+int main()
+{ 
+	string str; 
+	while (getline(cin, str))
+	{ 
+		int count = 0;
+		//首先计算参数数量
+		for (int i = 0; i < str.size(); i++) 
+		{ 
+			if (str[i] == ' ') 
+				count++; 
+			//如果是双引号，向后遍历，直到下一个双引号结束
+			if (str[i] == '"') 
+			{ 
+				do{i++;
+				}
+		while (str[i] != '"');
+			}
+		}
+		//以空格计算个数，空格数量比参数个数少1
+		cout << count + 1 << endl; 
+		//用flag表示是否包含双引号，0表示有双引号
+		//双引号中的空格要打印出来
+		//用异或改变flag的值，两个双引号可以使flag复原
+		int flag = 1;
+		for (int i = 0; i < str.size(); i++)
+		{ 
+			//有双引号，flag通过异或变为0，下一次再遇到双引号，flag置为1
+			if (str[i] == '"')
+				flag ^= 1; 
+			//双引号和普通空格不打印
+			if (str[i] != ' ' && str[i] != '"')
+				cout << str[i];
+			//双引号中的空格要打印 
+			if (str[i] == ' ' && (!flag)) 
+				cout << str[i];
+			//遇到双引号之外的空格就换行
+			if (str[i] == ' ' && flag) 
+				cout << endl;
+		}
+		cout << endl; 
+	}
+	return 0;
+}
